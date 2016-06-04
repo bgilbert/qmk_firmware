b10a6e91d596ca710adcc2851fcdc43b17207ffa
Benjamin Gilbert <bgilbert@backtick.net>
2016-06-04 15:25:28 -0500

    Add remaining shifted symbols to symbol layer
    to reduce confusion over which meta key to use.  Also rearrange a few
    symbols.

--- keyboard/ergodox_ez/keymaps/bgilbert/keymap.c
+++ keyboard/ergodox_ez/keymaps/bgilbert/keymap.c
@@ -57,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |  !   |  @   |  #   |  $   |  %   |      |           |      |      |  =   |  -   |  _   |  +   |   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |  ^   |  &   |  *   |  (   |  )   |------|           |------|      |  {   |  }   |  [   |  ]   |        |
+ * |        |  ^   |  &   |  *   |  ;   |  :   |------|           |------|      |  {   |  }   |  [   |  ]   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |  '   |  "   |  |   |  \   |        |
+ * |        |  \   |  |   |  "   |  '   |  ?   |      |           |      |      |  (   |  )   |  <   |  >   |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -76,8 +76,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // left hand
        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
        KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TRNS,
-       KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_SCLN, KC_COLN,
+       KC_TRNS, KC_BSLS, KC_PIPE, KC_DQUO, KC_QUOT, KC_QUES, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                        KC_TRNS, KC_TRNS,
                                                 KC_TRNS,
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_TRNS, KC_EQL,  KC_MINS, KC_UNDS, KC_PLUS, KC_F12,
                 KC_TRNS, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_QUOT, KC_DQUO, KC_PIPE, KC_BSLS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, KC_LT,   KC_GT,   KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
