195b946ba16b740d54f92f7781692cb1baa91989
Benjamin Gilbert <bgilbert@backtick.net>
2016-06-05 01:54:38 -0500

    Swap braces and parens

--- keyboard/ergodox_ez/keymaps/bgilbert/keymap.c
+++ keyboard/ergodox_ez/keymaps/bgilbert/keymap.c
@@ -57,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |  !   |  @   |  #   |  $   |  %   |      |           |      |      |  =   |  -   |  _   |  +   |   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |  ^   |  &   |  *   |  ;   |  :   |------|           |------|      |  {   |  }   |  [   |  ]   |        |
+ * |        |  ^   |  &   |  *   |  ;   |  :   |------|           |------|      |  (   |  )   |  [   |  ]   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |  \   |  |   |  "   |  '   |  ?   |      |           |      |      |  (   |  )   |  <   |  >   |        |
+ * |        |  \   |  |   |  "   |  '   |  ?   |      |           |      |      |  {   |  }   |  <   |  >   |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -85,8 +85,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // right hand
        KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_TRNS, KC_EQL,  KC_MINS, KC_UNDS, KC_PLUS, KC_F12,
-                KC_TRNS, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, KC_LT,   KC_GT,   KC_TRNS,
+                KC_TRNS, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_LT,   KC_GT,   KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
